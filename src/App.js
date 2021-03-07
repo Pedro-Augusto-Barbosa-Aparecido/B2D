796eaeb9c0c9f7dbd8c6113d98a9a68cc9fd8627
Pedro-Augusto-Barbosa-Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2021-03-06 21:49:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { convertNumber } from './modules/convert';
+
 function App() {
+
+  const [binState, setBinState] = useState("no bin")
+  const [decimal, setDecimal] = useState(" ")
+  const [message, setMessage] = useState("")
+
+  var i = 0
+
+  const success = () => {
+    
+    setDecimal(convertNumber(binState, binState.length))
+    setMessage("") 
+
+  }
+
+  const erro = () => {
+ 
+    if (i === 0) {
+      setMessage("Número digitado inválido")
+      setDecimal("")
+      i += 1
+    } else {
+      setMessage("Número digitado inválido")
+      setDecimal("")
+      alert(message)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+
+      <div
+        className="TextContainer"
+      >
+        <p
+          className="Text"
+        >Digite um valor em binário, com no máximo 8 digitos: { decimal } <br /></p>
+
+        <p 
+          className="Text"
+        >{message}</p>
+      </div>
+
+      <div 
+        className="InputBoxContainer"
+      >
+        <input 
+          className="InputBox"
+          placeholder="Digite o valor em binário..."
+          type="text"
+          maxLength="8"
+          onChange={(value) => { setBinState(value.target.value) }}
+        />
+      </div>
+
+      <div
+        className="ButtonBoxContainer"
+      >
+        <button
+          className="ButtonBox"
+          onClick={() => { 
+            convertNumber(binState, binState.length) ? success() : erro()
+          }}
+        >Converter</button>
+      </div>
+
     </div>
   );
 }
---
+++ src/modules/convert.js
@@ -0,0 +1,56 @@
+function convertNumber(bin, length) {
+
+    let counter = 0
+    let _bases = []
+    let bases = []
+
+    let decimal = 0
+
+    initBasesArray()
+
+    for (var i = 0; i < length; i++) {
+        if (bin[i] === "0") { 
+            counter += 1
+        } else if (bin[i] === "1") {
+            counter += 1
+        }
+    }
+
+    if (counter !== length) {
+        return false
+    }
+
+    for (var w = length; w >= 0; w--) {
+        
+        if (bin[w] === "1") {
+            decimal += bases[w]
+        }
+
+    }
+
+    
+
+    function initBasesArray() {
+
+        var base = 1
+
+        _bases.push(base)
+
+        for (var i = 0; i < length - 1; i++) {
+            _bases.push(base * 2)
+            base *= 2
+        }
+
+        for (var a = _bases.length - 1; a >= 0; a--) {
+            bases.push(_bases[a])
+        }
+
+    }
+
+    return decimal
+
+}
+
+module.exports = {
+    convertNumber,
+}
